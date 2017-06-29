b6d2f73db3f520d380dfafb51abd6dab18da859c
john <hielis@localhost.localdomain>
2017-06-29 11:54:11 +0100

    Localisation in space works, to do : 	-> display points in space 	-> wrapper

--- mysite/mesh/recog.py
+++ mysite/mesh/recog.py
@@ -30,8 +30,8 @@ list_of_points = [1, #ear left
 				59]; #list here the interesting features
 
 alpha = 0.3926
-ratio_mm_pix_x = 1/4.44
-ratio_mm_pix_y = 1/4.44
+ratio_mm_pix_x = 4.44
+ratio_mm_pix_y = 4.44
 focal_length = 35
 b = 2*ratio_mm_pix_x
 
@@ -43,41 +43,35 @@ predictor = dlib.shape_predictor(predictor_path)
 
 
 def funct(f):
-    print("Processing file: {}".format(f))
+    #print("Processing file: {}".format(f))
     img = Image.open(f).convert('RGB')
     imgp = np.array(img)
     dets = detector(imgp, 1)
     l = []
-    print len(dets)
+    #print len(dets)
     for k, d in enumerate(dets):
         shape = predictor(imgp, d)
         l = [shape.part(i) for i in list_of_points]
     return l
 
 def solve_system(x_r, y_r, x_l, y_l):
-    delta_x_l = (x_l - x_0)/focal_length
-    delta_y_l = (y_l - y_0)/focal_length
-    delta_x_r = (x_r - x_0)/focal_length
-    delta_y_r = (y_r - y_0)/focal_length
-    print delta_x_l
-    print delta_x_r
-    bp = np.array([b * delta_x_l, b * delta_y_l, b * delta_x_r])
-    a = np.array([[ (delta_x_l - 1) * math.cos(alpha), 0, (delta_x_l + 1) * math.sin(alpha)],
-                  [ (delta_y_l) * math.cos(alpha),-1, (delta_y_l) * math.sin(alpha)],
-                  [ (delta_x_r - 1) * math.cos(alpha), 0, (delta_x_r + 1) * math.sin(alpha)]])
-   # print a
-    print bp
-    h = np.linalg.solve(a, bp)
-    print a.dot(h)
-    return h
+	delta_x_1 = (x_r - x_0) / (2 * focal_length)
+	delta_x_2 = (x_l - x_0) / (2 * focal_length)
+	delta_y_1 = (y_r - y_0) / (2 * focal_length)
+	delta_y_2 = (y_l - y_0) / (2 * focal_length)
+	x = - (b / (2 * focal_length * math.cos(alpha))) * (delta_x_1*(1 - (delta_x_1 / focal_length)) + delta_x_2*(1 - (delta_x_2 / focal_length)))
+	z = (b / (2 * focal_length * math.sin(alpha))) * (delta_x_1*(1 - (delta_x_1 / focal_length)) - delta_x_2*(1 - (delta_x_2 / focal_length)))
+	y =  - (delta_y_1 * b / focal_length) * (1 - (1 / focal_length)*(1 / ((delta_x_1 / focal_length) - 1)))
+	print (x, y, z) 
+	return (x, y, z) 
 
 def solve(x_r, y_r, x_l, y_l):
     x_r_p = x_r * ratio_mm_pix_x
     y_r_p = y_r * ratio_mm_pix_y
     x_l_p = x_l * ratio_mm_pix_x
     y_l_p = y_r * ratio_mm_pix_y
-    return (solve_system(x_r_p, y_r_p, x_l_p, y_l_p))*(1/ratio_mm_pix_y)
-
+    (x, y, z) = (solve_system(x_r_p, y_r_p, x_l_p, y_l_p))
+    return (x*(1/ratio_mm_pix_y), y*(1/ratio_mm_pix_y), z*(1/ratio_mm_pix_y))
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -87,8 +81,8 @@ if __name__ == "__main__":
 	arg = parser.parse_args()
 	l_r = funct(arg.filename_r)
 	l_l = funct(arg.filename_l)
-	p = 16
+	p = 11
 	pair = [l_r[p].x, l_r[p].y, l_l[p].x, l_l[p].y]
-	#print pair
-	print "Solution"
+	print pair
+	#print "Solution"
 	print solve(pair[0], pair[1], pair[2], pair[3])
